f54948d07eca852b2efb725db08ebb41806c3bf8
Freddy Kristiansen <fkr@freddy.dk>
2026-05-18 07:37:54 +0200

    Add SetupNavUsers override

---
+++ ContainerScripts/SetupNavUsers.ps1
@@ -0,0 +1,27 @@
+# INPUT
+#     $auth
+#     $username (optional)
+#     $securePassword (optional)
+#
+# OUTPUT
+#
+
+if ($auth -eq "Windows") {
+    if ($username -ne "") {
+        if (!(Get-NAVServerUser -ServerInstance $ServerInstance @tenantParam -ErrorAction Ignore | Where-Object { $_.UserName.EndsWith("\$username", [System.StringComparison]::InvariantCultureIgnoreCase) -or $_.UserName -eq $username })) {
+            Write-Host "Creating SUPER user"
+            New-NavServerUser -ServerInstance $ServerInstance @tenantParam -WindowsAccount $username
+            New-NavServerUserPermissionSet -ServerInstance $ServerInstance @tenantParam -WindowsAccount $username -PermissionSetId SUPER
+        }
+    }
+} else {
+    if (!(Get-NAVServerUser -ServerInstance $ServerInstance @tenantParam -ErrorAction Ignore | Where-Object { $_.UserName -eq $username })) {
+        Write-Host "Creating SUPER user"
+        New-NavServerUser -ServerInstance $ServerInstance @tenantParam -Username $username -Password $securePassword -AuthenticationEMail $authenticationEMail
+        New-NavServerUserPermissionSet -ServerInstance $ServerInstance @tenantParam -username $username -PermissionSetId SUPER
+    }
+    else {
+        Write-Host "SUPER user already exists"
+        Set-NavServerUser -ServerInstance $ServerInstance @tenantParam -Username $username -Password $securePassword
+    }
+}
